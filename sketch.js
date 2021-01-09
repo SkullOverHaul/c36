4d7fe628f0ca6bdc55703bb1aac06ad78f81b884
SkullOverHaul <72302535+SkullOverHaul@users.noreply.github.com>
2021-01-09 12:56:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var ball;
+var database,position;
+var gameState=0;
+var playerCount,form,player,game;
+
+function setup(){
+    database = firebase.database();
+
+    createCanvas(400,400);
+    game = new Game()
+    game.getState()
+    game.start()
+}
+
+function draw(){
+
+
+}
